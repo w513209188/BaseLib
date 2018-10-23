8f267e6c1e5302a526a8a8f69f72f6a21dc9e098
王辉 <513209188@qq.com>
2018-10-23 15:21:44 +0800

    frist commit

---
+++ app/src/main/java/com/example/seele/myapplication/mApp.java
@@ -0,0 +1,10 @@
+package com.example.seele.myapplication;
+
+import com.wb.baselib.BaseApplication;
+
+public class mApp extends BaseApplication {
+    @Override
+    public String getRootPackAge() {
+        return "fsfsfs";
+    }
+}
