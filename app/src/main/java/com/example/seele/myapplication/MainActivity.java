9b1f7226b075ec3726e94748ac0c652e6bee8b59
王辉 <513209188@qq.com>
2018-11-06 15:36:37 +0800

    fix bug

--- app/src/main/java/com/example/seele/myapplication/MainActivity.java
+++ app/src/main/java/com/example/seele/myapplication/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.seele.myapplication;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.wb.baselib.crash.CrashHandler;
+import com.wb.baselib.http.HttpConfig;
+import com.wb.baselib.http.HttpManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity{
     private Button post_bt;
 
 
-//    @Override
-//    protected void onCreate(@Nullable Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        post_bt=this.findViewById(R.id.post_bt);
-//        post_bt.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        CrashHandler.getInstance().CheckAppCarchLog();
+        post_bt=this.findViewById(R.id.post_bt);
+        post_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                post_bt.setText(null);
 //                Map<String,String> map=new HashMap<>();
 //                map.put("dsfds","fsdfds");
 //                new HttpConfig.HttpConfigBuilder()
@@ -26,14 +36,14 @@ public class MainActivity extends AppCompatActivity{
 //                        .setIsReshConfig(true)
 //                        .setmMapHeader(map)
 //                        .setmIsUseLog(false).build();
-//                Log.e("huoqu",HttpManager.newInstance().getHttpConfig().getmMapHeader().toString());
-//            }
-//        });
-//    }
+//                Log.e("huoqu", HttpManager.newInstance().getHttpConfig().getmMapHeader().toString());
+            }
+        });
+    }
 
 
     protected void initView(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_test);
+
     }
 
 }
---
+++ app/src/main/java/com/example/seele/myapplication/mApp.java
@@ -0,0 +1,41 @@
+package com.example.seele.myapplication;
+
+import com.wb.baselib.BaseApplication;
+import com.wb.baselib.appconfig.AppConfig;
+import com.wb.baselib.appconfig.AppConfigManager;
+import com.wb.baselib.crash.CrashHandler;
+import com.wb.baselib.http.HttpConfig;
+import com.wb.baselib.log.LogTools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class mApp extends BaseApplication{
+    @Override
+    public String getRootPackAge() {
+        return "dsdsfsd";
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        List<Integer> httpCodeOff = new ArrayList<>();
+        httpCodeOff.add(201);
+        AppConfig appConfig = new AppConfig.Bulider()
+                .setMaxPage(10)
+                .setHttpCodeOff(httpCodeOff)
+                .setSendCarshLog(true)
+                .setCatshLog(true)
+                .setSendCarshLog(true)
+                .bulider();
+        AppConfigManager.newInstance().setAppConfig(appConfig);
+        LogTools.setDebug(true);
+
+        HttpConfig.HttpConfigBuilder httpConfig =
+                new HttpConfig.HttpConfigBuilder()
+                        .setmBaseUrl("http://test-px.huatu.com")
+                        .setmIsUseLog(true);
+        HttpConfig.newInstanceBuild(httpConfig);
+        CrashHandler.getInstance().init(this);
+    }
+}
